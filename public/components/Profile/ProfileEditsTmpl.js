f0e6288f18e0d2bfcf1f4fa84928f06cadcc96ff
labzunova <dasha.labzunova@mail.ru>
2021-03-14 15:56:58 +0300

    password updating added

--- public/components/Profile/ProfileEditsTmpl.js
+++ public/components/Profile/ProfileEditsTmpl.js
@@ -7,6 +7,12 @@ export const renderProfileEdits = window.Handlebars.compile(`
     <p id="emailError" class="error">Почта</p>
     <input type="text" id="name" class="edit-input" placeholder="Имя" name="name" value={{ user.name }}>
     <p id="nameError" class="error">Имя</p>
+    <input type="password" id="password_current" class="edit-input" placeholder="Текущий пароль" name="password_current" autocomplete="on">
+    <p id="repeatPasswordError" class="error">Текущий пароль</p>
+    <input type="password" id="password" class="edit-input" placeholder="Новый пароль" name="password" autocomplete="on">
+    <p id="passwordError" class="error">Новый пароль</p>
+    <input type="password" id="password_repeat" class="edit-input" placeholder="Повторите пароль" name="password_repeat" autocomplete="on">
+    <p id="repeatPasswordError" class="error">Повторите пароль</p>
     <img src="{{ user.avatar }}" class="profile-avatar" alt="ava" id="current_ava"/>
     <input type="file" id="avatar" name="avatar" accept="image/*"><br/><br/>
     <input type="submit" class="button-log" value="Изменить данные">

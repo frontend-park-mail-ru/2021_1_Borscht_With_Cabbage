0e733f4417b42455a1f03a54ec2bea8c4a1fb4ef
warlikegh <a_katnov@mail.ru>
2021-04-04 10:06:07 +0300

    right basket

--- public/components/DeliveryBasket/DeliveryBasket.js
+++ public/components/DeliveryBasket/DeliveryBasket.js
@@ -1,4 +1,10 @@
 import { noop } from '../../modules/utils.js';
+import {
+    renderDeliveryBasketBody,
+    renderDeliveryBasketFooter,
+    renderDeliveryBasketHeader
+} from './DeliveryBasketTmpl.js';
+import { renderStoreBasketFood } from '../StoreBasket/StoreBasketFood/StoreBasketFoodTmpl.js';
 
 export class DeliveryBasket {
     constructor ({
@@ -10,6 +16,21 @@ export class DeliveryBasket {
     }
 
     render (info) {
+        this.root.insertAdjacentHTML('beforeend', renderDeliveryBasketHeader({
+            store: info
+        }));
+        this.root.insertAdjacentHTML('beforeend', renderDeliveryBasketBody({}));
+        this.root.insertAdjacentHTML('beforeend', renderDeliveryBasketFooter({
+            store: info
+        }));
 
+        const body = this.root.querySelector('.delivery-basket__body');
+        this.foodList = info.foods;
+        this.foodList.forEach(element => {
+            body.insertAdjacentHTML('beforeend', renderStoreBasketFood({
+                chosenDish: element,
+                num: element.num
+            }));
+        });
     }
 }

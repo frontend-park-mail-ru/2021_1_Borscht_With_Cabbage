1cde69cf19dcd1aeca561ae3c0a1a428aed42984
warlikegh <a_katnov@mail.ru>
2021-04-11 14:01:44 +0300

    local storage basket

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,5 @@
 const KEY = 'BORSCHT_WITH_CABBAGE_CACHE';
+const LOCAL_STORAGE_BASKET = 'LOCAL_STORAGE_BASKET';
 
 self.addEventListener('install', event => {
     event.waitUntil(self.skipWaiting());
@@ -17,13 +18,13 @@ self.addEventListener('message', event => {
     }
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
+        caches.keys().then(function (cacheNames) {
             return Promise.all(
-                cacheNames.filter(function() {
+                cacheNames.filter(function () {
                     return true;
-                }).map(function(cacheName) {
+                }).map(function (cacheName) {
                     return caches.delete(cacheName);
                 })
             );
@@ -36,47 +37,111 @@ self.addEventListener('fetch', event => {
     const url = new URL(request.url);
 
     event.respondWith(  // отменяем поведение по умолчанию
-        caches
-            .match(request) // промис, который ищет закэшированный респонс
-            .then(async cachedResponse => {
-                if (navigator.onLine) { // если онлайн, то сходим в нет
-                    if (!url.href.includes(':5000')) {
-                        const response = await fetch(request);
-                        const respClone = response.clone(); // респонс -- поток и мы можем прочитать его лишь раз (иначе не закэшировать)
-                        caches
-                            .open(KEY)
-                            .then(cache => cache.put(request, respClone));
-                        return response;
+        new Promise(() => {         // модифицируем реквест в соответствие с
+            if (url.href.includes('/user/basket')) {
+                if (request.method.toUpperCase() === 'PUT') { // запрос на добавление в корзину
+                    const body = JSON.parse(request.body);
+                    const isAuth = body.isAuth;
+                    if (!isAuth) {     // if user not auth
+                        const dishID = body.dishID;
+                        const restaurantID = body.restaurantID;
+                        const basketString = localStorage[LOCAL_STORAGE_BASKET];
+                        const basket = basketString ? JSON.parse(basketString) : {};
+                        if (basket.restaurantID && basket.restaurantID === restaurantID) {
+                            const isPlus = body.isPlus;
+                            switch (isPlus) {
+                            case true:          // plus
+                                let isFind = false;
+                                for (const food of basket.foods) {
+                                    if (food.id === dishID) {
+                                        food.num += 1;
+                                        isFind = true;
+                                        break;
+                                    }
+                                }
+                                if (!isFind) {
+                                    basket.foods.push({
+                                        id: dishID,
+                                        num: 1
+                                    });
+                                }
+                                break;
+                            case false:          // minus
+                                let isDelete = false;
+                                let deleteFood = null;
+                                for (const food of basket.foods) {
+                                    if (food.id === dishID) {
+                                        food.num -= 1;
+                                        if (food.num === 0) {
+                                            isDelete = true;
+                                            deleteFood = food;
+                                        }
+                                        break;
+                                    }
+                                }
+                                if (isDelete) {
+                                    basket.foods = basket.foods.filter(value => value !== deleteFood)
+                                }
+                            }
+                        } else {
+                            basket.restaurantID = restaurantID;
+                            basket.foods = [];
+                            basket.foods.push({
+                                id: dishID,
+                                num: 1
+                            });
+                        }
+                        localStorage[LOCAL_STORAGE_BASKET] = JSON.stringify(basket)
+                    } else {    // if user auth
+                        return fetch(request);
                     }
+                } else if (request.method.toUpperCase() === 'POST') { // запрос на формирование полной коризны
+                    request.body = localStorage[LOCAL_STORAGE_BASKET];
                     return fetch(request);
                 }
-
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                if (url.href.includes(':5000')) {
-                    const init = {
-                        status: 200
-                    };
-
-                    const data = { code: 418, message: 'Контент недоступен в оффлайн режиме' };
-                    const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
-                    return new Response(blob, init);
-                } else {
-                    const baseUrl = url.toString().replace(url.pathname, '/');
-                    try {
-                        const cache = await caches.open(KEY);
-                        const keys = await cache.keys();
-                        const request = keys.find(key => key.url.toString() === baseUrl);
-                        return await caches.match(request);
-                    } catch (e) {
-                        console.log(e);
+            }
+        }).then(() =>
+            caches
+                .match(request) // промис, который ищет закэшированный респонс
+                .then(async cachedResponse => {
+                    if (navigator.onLine) { // если онлайн, то сходим в нет
+                        if (!url.href.includes(':5000')) {
+                            const response = await fetch(request);
+                            const respClone = response.clone(); // респонс -- поток и мы можем прочитать его лишь раз (иначе не закэшировать)
+                            caches
+                                .open(KEY)
+                                .then(cache => cache.put(request, respClone));
+                            return response;
+                        }
+                        return fetch(request);
                     }
-                }
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    );
+
+                    if (cachedResponse) {
+                        return cachedResponse;
+                    }
+
+                    if (url.href.includes(':5000')) {
+                        const init = {
+                            status: 200
+                        };
+
+                        const data = { code: 418, message: 'Контент недоступен в оффлайн режиме' };
+                        const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+                        return new Response(blob, init);
+                    } else {
+                        const baseUrl = url.toString().replace(url.pathname, '/');
+                        try {
+                            const cache = await caches.open(KEY);
+                            const keys = await cache.keys();
+                            const request = keys.find(key => key.url.toString() === baseUrl);
+                            return await caches.match(request);
+                        } catch (e) {
+                            console.log(e);
+                        }
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        ));
 });

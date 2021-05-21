225e59c06c078ba9dd9756e218e2fdcbb6cf019e
Sergei Shishkin <3shishkin39@mail.ru>
2021-05-21 19:52:41 +0300

    http -> https in request

--- public/modules/http.js
+++ public/modules/http.js
@@ -1,7 +1,7 @@
 import eventBus from './eventBus.js';
 import { AuthEvents } from 'Events/AuthEvents.js';
 
-window.serverAddress = 'http://delivery-borscht.ru/api';
+window.serverAddress = 'https://delivery-borscht.ru/api';
 //  window.serverAddress = 'http://127.0.0.1:5000/api'
 
 function getParams ({
